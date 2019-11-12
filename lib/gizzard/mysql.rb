b1f5e57268b8e68402b524f9023acd137ea09de7
Takahiro Ooishi <taka0125@gmail.com>
2019-11-12 17:46:56 +0900

    :+1: use index hint when join

--- lib/gizzard/mysql.rb
+++ lib/gizzard/mysql.rb
@@ -30,8 +30,54 @@ module Gizzard
         from("#{table_name} FORCE INDEX(#{Array(indexes).join(', ')})")
       end
 
+      def joins_with_use_index(relation_name, indexes)
+        joins_with_index_hint(relation_name, indexes, join_type: :inner_join, hint: :use)
+      end
+
+      def joins_with_force_index(relation_name, indexes)
+        joins_with_index_hint(relation_name, indexes, join_type: :inner_join, hint: :force)
+      end
+
+      def left_outer_joins_with_use_index(relation_name, indexes)
+        joins_with_index_hint(relation_name, indexes, join_type: :left_outer_join, hint: :use)
+      end
+
+      def left_outer_joins_with_force_index(relation_name, indexes)
+        joins_with_index_hint(relation_name, indexes, join_type: :left_outer_join, hint: :force)
+      end
+
       def lock_in_share
         lock('LOCK IN SHARE MODE')
+      end
+
+      private
+
+      def joins_with_index_hint(relation_name, indexes, join_type: :inner_join, hint: :use)
+        relation = reflections[relation_name.to_s]
+        join_table_name = relation.klass.table_name
+
+        join = case join_type
+               when :inner_join
+                 'INNER JOIN'
+               when :left_outer_join
+                 'LEFT OUTER JOIN'
+               else
+                 raise
+               end
+
+        index_hint = case hint
+                     when :use
+                       'USE INDEX'
+                     when :force
+                       'FORCE INDEX'
+                     else
+                       raise
+                     end
+
+        c = connection
+        joins <<-SQL
+        #{join} #{c.quote_table_name(join_table_name)} #{index_hint} (#{Array(indexes).join(', ')}) ON #{c.quote_table_name(table_name)}.#{c.quote_column_name(relation.association_primary_key)} = #{c.quote_table_name(join_table_name)}.#{c.quote_column_name(relation.foreign_key)}
+        SQL
       end
     end
 
